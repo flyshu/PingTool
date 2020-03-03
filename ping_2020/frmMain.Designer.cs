170d23c5d989a2b9d5cd411e1b520f42bb71b384
flyshu@qq.com <flyshu@qq.com>
2020-03-03 08:53:35 +0800

    添加项目文件。

---
+++ ping_2020/frmMain.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ping_2020 {
+	partial class frmMain {
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.menuMethods = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuMethodsStart = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuMethodsStop = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStripText = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.ToolStripMenuItemStart = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemStop = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItemColor = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.contextMenuStripText.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuMethods});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1421, 25);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "操作";
+			// 
+			// menuMethods
+			// 
+			this.menuMethods.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuMethodsStart,
+            this.menuMethodsStop});
+			this.menuMethods.Name = "menuMethods";
+			this.menuMethods.Size = new System.Drawing.Size(44, 21);
+			this.menuMethods.Text = "操作";
+			// 
+			// menuMethodsStart
+			// 
+			this.menuMethodsStart.Name = "menuMethodsStart";
+			this.menuMethodsStart.Size = new System.Drawing.Size(180, 22);
+			this.menuMethodsStart.Text = "全部启动";
+			this.menuMethodsStart.Click += new System.EventHandler(this.menuMethodsStart_Click);
+			// 
+			// menuMethodsStop
+			// 
+			this.menuMethodsStop.Name = "menuMethodsStop";
+			this.menuMethodsStop.Size = new System.Drawing.Size(180, 22);
+			this.menuMethodsStop.Text = "全部停止";
+			this.menuMethodsStop.Click += new System.EventHandler(this.menuMethodsStop_Click);
+			// 
+			// contextMenuStripText
+			// 
+			this.contextMenuStripText.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemStart,
+            this.toolStripMenuItemStop,
+            this.toolStripMenuItemColor});
+			this.contextMenuStripText.Name = "contextMenuStripText";
+			this.contextMenuStripText.Size = new System.Drawing.Size(181, 92);
+			// 
+			// ToolStripMenuItemStart
+			// 
+			this.ToolStripMenuItemStart.Enabled = false;
+			this.ToolStripMenuItemStart.Name = "ToolStripMenuItemStart";
+			this.ToolStripMenuItemStart.Size = new System.Drawing.Size(180, 22);
+			this.ToolStripMenuItemStart.Text = "开始";
+			this.ToolStripMenuItemStart.Click += new System.EventHandler(this.ToolStripMenuItemStart_Click);
+			// 
+			// toolStripMenuItemStop
+			// 
+			this.toolStripMenuItemStop.Name = "toolStripMenuItemStop";
+			this.toolStripMenuItemStop.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItemStop.Text = "停止";
+			this.toolStripMenuItemStop.Click += new System.EventHandler(this.toolStripMenuItemStop_Click);
+			// 
+			// toolStripMenuItemColor
+			// 
+			this.toolStripMenuItemColor.Name = "toolStripMenuItemColor";
+			this.toolStripMenuItemColor.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItemColor.Text = "改变状态";
+			this.toolStripMenuItemColor.Click += new System.EventHandler(this.toolStripMenuItemColor_Click);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1421, 668);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "frmMain";
+			this.Text = "服务器实时网络状态监控程序";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.Resize += new System.EventHandler(this.frmMain_Resize);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.contextMenuStripText.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem menuMethods;
+		private System.Windows.Forms.ToolStripMenuItem menuMethodsStart;
+		private System.Windows.Forms.ToolStripMenuItem menuMethodsStop;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStripText;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemStart;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemStop;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemColor;
+	}
+}
+
---
+++ ping_2020/frmMain.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using ping_2020.AuthorityService;
+using ping_2020.messageService;
+
+namespace ping_2020 {
+	public partial class frmMain : Form {
+
+
+		private delegate void ChangeTextCallback(string value, int id, string color = "black");
+		private ChangeTextCallback ChangeTextInvoke;
+		Int32 hostCount = 1; //定级主机数量
+		GroupBox[] groupBoxs = new GroupBox[1]; //包裹txt的外框
+		TextBox[] TxtPings = new TextBox[1];//用于输入ping的文本框
+		Thread[] thread = new Thread[1];//多线程
+		Int32[] ProcessStatus = new Int32[1]; //线程状态
+		Hashtable Hosts = new Hashtable();
+		//
+		public string token_id;
+
+		public frmMain() {
+			InitializeComponent();
+			this.Text = "服务器实时网络状态监控程序 Ver:" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+			//
+			Hosts.Add("baidu.com", "baidu.com");
+			Hosts.Add("大共享服务器", "172.16.32.1");
+			Hosts.Add("考勤服务器", "172.16.32.2");
+			Hosts.Add("云服务器1", "172.16.32.9");
+			Hosts.Add("云服务器2", "172.16.32.10");
+			Hosts.Add("设计部服务器", "172.16.32.35");
+			Hosts.Add("新档案服务器", "172.16.32.18");
+			Hosts.Add("致远OA服务器", "172.16.33.140");
+			Hosts.Add("新OA数据服务器", "172.16.32.22");
+			Hosts.Add("总管理处共享", "172.16.33.67");
+			Hosts.Add("总体行销服务器", "172.16.33.43");
+			Hosts.Add("RedMine服务器", "172.16.32.33");
+			Hosts.Add("财务用友服务器", "172.16.33.59");
+			hostCount = Hosts.Count;
+			groupBoxs = new GroupBox[hostCount];
+			TxtPings = new TextBox[hostCount];
+			thread = new Thread[hostCount];
+			ProcessStatus = new Int32[hostCount];
+			//
+			AuthorityService.authorityService ws = new AuthorityService.authorityService();
+			AuthorityService.UserToken token = ws.authenticate("service-admin", "123456");
+			Console.WriteLine(token.id);
+			token_id = token.id;
+
+
+		}
+
+		private void SendOAMessage(string content) {
+			List<string> strList = new List<string>();
+			strList.Add("B01388");
+			strList.Add("B01329");
+			List<string> urlList = new List<string>();
+			urlList.Add("https://www.baidu.com");
+			string[] astr = strList.ToArray();
+			string[] url = urlList.ToArray();
+			try {
+				messageService.messageService msg = new messageService.messageService();
+				msg.sendMessageByLoginNameAsync(token_id, astr, content, url);
+
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+
+		private void SetValue(string value, int id, string color = "black") {
+			if (color == "red") {
+				this.TxtPings[id].BackColor = System.Drawing.Color.Red;
+			} else {
+				this.TxtPings[id].BackColor = System.Drawing.Color.Black;
+			}
+			this.TxtPings[id].Text += value.ToString() + Environment.NewLine;
+			this.TxtPings[id].SelectionStart = this.TxtPings[id].Text.Length;
+			this.TxtPings[id].ScrollToCaret();
+		}
+
+		private Int32 ReturnMs(string str) {
+			int pos1, pos2;
+			string str1 = "时间", str2 = "ms", result = "";
+			if (str.Length == 0)
+				return -1;
+			pos1 = str.IndexOf(str1);
+			if (pos1 < 0)
+				return -1;
+			pos2 = str.IndexOf(str2, pos1);
+			if (pos2 == 0) {
+				return -1;
+			}
+
+			result = str.Substring(pos1 + 3, pos2 - pos1 - 3);
+			return Convert.ToInt32(result);
+		}
+		private void Ping(object obj) {
+			//Process[] P =new Process[hostCount];
+			string[] strs = (string[])obj;
+			int id = Convert.ToInt32(strs[0]);
+			string url = strs[1];
+			string name = strs[2];
+			Process P;
+			string readline = "";
+			Int32 ms;
+			Int32 errNum = 0, normalNum = 0;
+			P = new Process();
+			P.StartInfo.FileName = "ping";
+			P.StartInfo.Arguments = url + " -t";
+			P.StartInfo.UseShellExecute = false;
+			P.StartInfo.RedirectStandardInput = true;
+			P.StartInfo.RedirectStandardOutput = true;
+			P.StartInfo.RedirectStandardError = true;
+			P.StartInfo.CreateNoWindow = true;
+			P.Start();
+			while (!P.HasExited) {
+				if (ProcessStatus[id] == 0) {
+					TxtPings[id].Invoke(ChangeTextInvoke, Environment.NewLine + "等待关闭进程" + ".", id, "");
+					P.Kill();
+					break;
+				} else if (!P.StandardOutput.EndOfStream) {
+					readline = P.StandardOutput.ReadLine();
+					ms = ReturnMs(readline);
+					if (ms == -1) {
+						errNum += 1;
+					} else if (ms > 100) {
+						errNum += 1;
+					} else {
+						normalNum += 1;
+					}
+					TxtPings[id].Invoke(ChangeTextInvoke, readline, id, "");
+				}
+				if (errNum > 5) {
+					errNum = 0;
+					normalNum = 0;
+					SendOAMessage(name + url + "网络超时超过5次");
+					TxtPings[id].Invoke(ChangeTextInvoke, "超时超过5次", id, "red");
+				}
+				if (normalNum > 20 && errNum==0) {
+					normalNum = 0;
+					//SendOAMessage(name + url + "恢复正常");
+					TxtPings[id].Invoke(ChangeTextInvoke, "正常状态", id, "black");
+				}
+			}
+
+			TxtPings[id].Invoke(ChangeTextInvoke, Environment.NewLine + "关闭成功", id, "");
+			thread[id].Abort();
+		}
+
+		private void menuMethodsStart_Click(object sender, EventArgs e) {
+			Int32 i = 0;
+			string[] para = new string[2];
+			foreach (DictionaryEntry host in Hosts) {
+				para = new string[3];
+				thread[i] = new Thread(new ParameterizedThreadStart(Ping));
+				ChangeTextInvoke = new ChangeTextCallback(SetValue);
+				ProcessStatus[i] = 1;
+				thread[i].IsBackground = true;
+				para[0] = i.ToString();
+				para[1] = host.Value.ToString();
+				para[2] = host.Key.ToString();
+				thread[i].Start(para);
+				i++;
+			}
+		}
+		private void menuMethodsStop_Click(object sender, EventArgs e) {
+			for (Int32 i = 0; i < hostCount; i++) {
+				TxtPings[i].AppendText(Environment.NewLine + "正在关闭thread");
+				//while (thread[i].IsAlive) {
+				ProcessStatus[i] = 0;
+				//Application.DoEvents();
+				//}
+			}
+		}
+		private void InitGroupAndText() {
+			Int32 x, y, i, width, height, txtWidth, txtHeight;
+			Int32 num_per_line = 5, lines = 4;
+			x = y = i = 0;
+			foreach (DictionaryEntry host in Hosts) {
+				width = (this.ClientRectangle.Width - 20) / num_per_line;
+				height = (this.ClientRectangle.Height - 20) / lines;
+				groupBoxs[i] = new GroupBox
+				{
+					Location = new Point(x * width + 20, height * y + 25),
+					Size = new System.Drawing.Size(width, height),
+					Name = "groups-" + i,
+					Text = host.Key.ToString() + " - " + host.Value.ToString(),
+					Tag = i
+				};
+				txtWidth = groupBoxs[i].Width - 10;
+				txtHeight = groupBoxs[i].Height - 20;
+				TxtPings[i] = new TextBox
+				{
+					Location = new Point(5, 15), //New Point(Form1.Label1.Text, Form1.Label2.Text)
+					Size = new System.Drawing.Size(width, height),
+					Name = "txt_ping-" + i, //TextBox1.Text
+					Tag = i,
+					Multiline = true,
+					Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left,
+					WordWrap = false,
+					ScrollBars = ScrollBars.Vertical,
+					ContextMenuStrip = contextMenuStripText,
+					ForeColor = Color.White,
+					BackColor = Color.Black,
+					ReadOnly = true
+				};
+				groupBoxs[i].Controls.Add(TxtPings[i]);
+				x += 1;
+				if (i % num_per_line == 0) {
+					y += 1;
+					x = 0;
+				}
+				this.Controls.Add(groupBoxs[i]);
+				i += 1;
+				//Console.WriteLine(groupBoxs[i].Location.ToString());
+			}
+		}
+
+
+
+		//载入config文件
+		private string LoadConfig() {
+			return "";
+		}
+
+		private void frmMain_Load(object sender, EventArgs e) {
+			InitGroupAndText();
+		}
+
+		private void frmMain_Resize(object sender, EventArgs e) {
+			Int32 display_rows = 5;
+			Int32 lines = 3;
+			Int32 i = 0, j = 0, line = 0;
+			Int32 width = (this.ClientRectangle.Width) / display_rows;
+			Int32 height = (this.ClientRectangle.Height) / lines;
+
+			//lines =(hostCount / display_rows);
+			//this.AutoScroll = true;
+
+			foreach (GroupBox group in groupBoxs) {
+				group.Width = width;
+				group.Height = height;
+				if (group.HasChildren) {
+					group.Controls[0].Width = width - 5;
+					group.Controls[0].Height = height - 5;
+				}
+				if (i % display_rows == 0) {
+					if (Convert.ToDouble(i / display_rows) > 0) {
+						j = 0;
+						line += 1;
+					}
+				}
+				group.Left = width * j;
+				group.Top = height * (line) + 25;
+				i++;
+				j++;
+			}
+		}
+
+		private void frmMain_FormClosing(object sender, FormClosingEventArgs e) {
+			DialogResult dr = MessageBox.Show("确定要退出吗？", "", MessageBoxButtons.OKCancel);
+			if (dr == DialogResult.OK) {
+				//KillAall();
+			} else {
+				e.Cancel = true;
+			}
+		}
+
+		private void ToolStripMenuItemStart_Click(object sender, EventArgs e) {
+
+		}
+
+		private void toolStripMenuItemStop_Click(object sender, EventArgs e) {
+			Int32 id = Convert.ToInt32(contextMenuStripText.SourceControl.Tag);
+			ProcessStatus[id] = 0;
+		}
+
+		private void toolStripMenuItemColor_Click(object sender, EventArgs e) {
+			Int32 id = Convert.ToInt32(contextMenuStripText.SourceControl.Tag);
+			TxtPings[id].BackColor = System.Drawing.Color.Black;
+		}
+	}
+}
